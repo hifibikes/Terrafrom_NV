491187630346d33d2c7e18ce788b8954a857047e
Suyog <suyog.mandwale@newvision-software.com>
2023-02-15 12:36:02 +0530

    rds code added

---
+++ Day4/main.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "name" {
+  allocated_storage    = 10
+  db_name              = var.db_name
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "admin@123"
+  skip_final_snapshot  = true
+}
