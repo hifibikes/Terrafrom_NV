2b4c005316d28c25c6b60ae4a7402a4db292ac3f
Suyog <suyog.mandwale@newvision-software.com>
2023-02-21 11:45:11 +0530

    file provisioner

--- Day5/main.tf
+++ Day5/main.tf
@@ -5,7 +5,21 @@ resource "aws_instance" "ec2" {
   instance_type = "t2.micro"
   ami = "ami-0aa7d40eeae50c9a9"
 
-  provisioner "local-exec" {
-   command = "echo ${self.private_ip} >> private_ip.txt"
- }
+#   provisioner "local-exec" {
+#    command = "echo ${self.private_ip} >> private_ip.txt"
+#  }
+
+  provisioner "file" {
+    source      = "http"
+    destination = "/home/ec2-user"
+     
+    connection {
+         user = "ec2-user"
+         type = "ssh"
+         private_key = file("NV_Mayank.pem")
+         host = self.public_ip
+    }
+
+  } 
+  
 }
