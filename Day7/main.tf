d92a9a9ef398ee346a454c3c28a25f90386aaefb
Suyog <suyog.mandwale@newvision-software.com>
2023-03-03 11:58:54 +0530

    3 backend

--- Day7/main.tf
+++ Day7/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "bucket-in28min-123123"
+    key    = "terrafrom_state_file/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
 resource "aws_instance" "public_ec2" {
   ami =  var.ami[0]
   instance_type = var.instance_type
